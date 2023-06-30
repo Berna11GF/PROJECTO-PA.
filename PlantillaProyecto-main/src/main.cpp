ddc8b7cbe83e2d385ba536e5a8ae6c6942c2f92a
Berna11GF <bernardo11gtzf@gmail.com>
2023-06-30 17:46:52 -0600

    Proyecto base

---
+++ PlantillaProyecto-main/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Clase.hpp>
+#include <ClaseB.hpp>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hola mundo: Github"<<endl;    
+    return 0;
+}
